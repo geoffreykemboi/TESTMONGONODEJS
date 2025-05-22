16e671dbba7f6aceea50c962bb4d01b884ece190
Geoffrey Kemboi <geoffreyk126@gmail.com>
2025-05-22 21:16:03 +0300

    Update atlas_uri.js

--- atlas_uri.js
+++ atlas_uri.js
@@ -1,2 +1,2 @@
-const uri = "mongodb+srv://geoffreykemboi:Kipchumba%4022@cluster0.fvvjo1i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://geoffreykemboi:<Paasword>@cluster0.fvvjo1i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 module.exports = uri;
